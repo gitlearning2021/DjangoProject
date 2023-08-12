e52c729c8dc883f3f0a0a15fb13b1cab488907fe
praveen <mllearning206@gmail.vom>
2023-08-12 18:29:27 +0530

    Hello World Program

--- DemoProject/DemoApplication/views.py
+++ DemoProject/DemoApplication/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def hello(request):
+    return HttpResponse('Hellow World')
+
+def about(request):
+    return HttpResponse('This is my first django application')
--- DemoProject/DemoProject/urls.py
+++ DemoProject/DemoProject/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path as url
+from DemoApplication import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('^$',views.hello,name='Hello'),
+    url('^about$',views.about,name='About')
 ]
